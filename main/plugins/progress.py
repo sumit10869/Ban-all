06a6a493f03485a263c9e6fa5215306e33dde4c5
sumit10869 <142525697+sumit10869@users.noreply.github.com>
2024-05-15 07:57:51 +0530

    Update progress.py

--- main/plugins/progress.py
+++ main/plugins/progress.py
@@ -5,8 +5,8 @@ import json
 from main.plugins.helpers import TimeFormatter, humanbytes
 
 #------
-FINISHED_PROGRESS_STR = "▊"
-UN_FINISHED_PROGRESS_STR = "▒"
+FINISHED_PROGRESS_STR = "🟢"
+UN_FINISHED_PROGRESS_STR = "🔴"
 DOWNLOAD_LOCATION = "/app"
 
 
